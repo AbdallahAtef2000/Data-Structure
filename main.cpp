0406af7b3934b8c44c40476e931120e4389daf48
AbdallahAtef2000 <161381609+AbdallahAtef2000@users.noreply.github.com>
2024-05-23 15:28:19 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,140 +1,108 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
+
 class Node{
-public:
+public :
     int data;
-    Node* next;
+    Node* ptr;
     Node(int val){
         this->data = val;
-        this->next= nullptr;
+        this->ptr = nullptr;
     }
 };
 
-
-class Linkedlist{
-public:
-    Node* head;
-    Linkedlist(){
-        head= NULL;
+class Stack{
+public :
+    Node* top;
+    Stack(){
+        top = nullptr;
     }
     bool isempty(){
-        return(head== nullptr);
+        return(top == nullptr);
     }
-    void insertfirst(int value){
-        Node *newnode = new Node(value);
-        newnode->next =head;
-        head = newnode;
+    void push(int item){
+        Node* newnode = new Node(item);
+        newnode->ptr = top;
+        top = newnode;
     }
-
-    void display(){
-        Node* temp = head;
-        cout<< "the content is"<<endl;
-        while(temp!= nullptr){
-            cout<< temp->data<<" ";
-            temp = temp->next;
-
-        }
-        cout<<"\n";
-    }
-
-    bool isfound(int item){
-        Node* temp = head;
-        while(temp != nullptr){
-           if (temp->data == item){
-            return true;
-            break;
-           }
-           else{
-            temp=temp->next;
-           }
-        }
-        return false;
-    }
-
-    int countnodes(){
-        Node* temp= head;
-        int counter=0;
-        while(temp != nullptr){
-            counter++;
-            temp = temp->next;
-        }
-        return counter;
-
-    }
-
-    void insertlast(int value){
-        Node* newnode = new Node(value);
-        Node* temp = head;
-        while (temp->next != nullptr){
-            temp = temp->next;
-        }
-        temp->next = newnode;
-
-    }
-
-    void insertbefore(int item, int newvalue){
-        Node* newnode = new Node(newvalue);
-        Node* temp= head;
-        while(temp->next->data != item && temp != nullptr){
-            temp = temp->next;
-        }
-        newnode->next = temp->next;
-        temp ->next = newnode;
-    }
-
-    void deletitem(int item){
-        Node* delptr = head;
-        delptr = delptr->next;
-        Node* preptr = head;
-        while(delptr!= nullptr && delptr->data != item){
-            delptr= delptr->next;
-            preptr = preptr->next;
-        }
-        preptr->next = delptr->next;
+    void pop(){
+        Node* delptr = top;
+        top = top->ptr;
         delete delptr;
     }
+    void display(){
+        Node* temp = top;
+        while (temp!= nullptr){
+            cout<< temp->data<< " ";
+            temp = temp->ptr;
+        }
+        delete temp;
+        cout<<endl;
+    }
+    int peek(){
+        return(top->data);
+    }
+    int countnodes(){
+        int counter = 0;
+        Node* temp = top;
+        while(temp!=nullptr){
+            counter++;
+            temp = temp->ptr;
+        }
+        return counter;
+        delete temp;
+
+    }
+    bool isfound(int item){
+        Node* temp = top;
+        while(temp!= nullptr){
+            if (temp->data == item){
+                return true;
+                break;
+            }
+            else{
+                temp = temp->ptr;
+            }
+        }
+        delete temp;
+    }
+    void isfull(){
+         Node* fulptr = new Node(0);
+         if(fulptr == nullptr){
+            cout<< "the stack is full"<<endl;
+         }
+         else {
+            cout<< " still have space in the memory"<<endl;
+         }
+         delete fulptr;
+    }
 
 
-};
+    };
 
 
 int main()
 {
-    int no_items;
-    int itema;
-    Linkedlist lst;
-    cout<<lst.isempty()<<endl;
-    cout<< "enter the no of values"<<endl;
-    cin>> no_items;
-    for (int i =0 ; i < no_items;i++){
-        cout<< "the item no "<< i+1 <<endl;
-        cin>>itema;
-        lst.insertfirst(itema);
+    Stack st1;
+    int item;
+    int no_of_items;
+    cout<< " The number of values to add "<<endl;
+    cin>> no_of_items;
+    for(int i =0 ; i < no_of_items; i++){
+        cout<< "the item no "<< i+1<<endl;
+        cin>>item;
+        st1.push(item);
     }
-    lst.display();
-    cout<< "value to add at the end of list"<<endl;
-    cin>>itema;
-    lst.insertlast(itema);
-    lst.display();
-    cout<< "the value to insert in the list"<<endl;
-    cin>>itema;
-    int pos;
-    cout<< "the item should be after this value "<<endl;
-    cin>>pos;
-    lst.insertbefore(pos,itema);
-    lst.display();
-    int delitem;
-    cout<< " the item you want to delete \n";
-    cin>>delitem;
-    lst.deletitem(delitem);
-    lst.display();
-    
-
-
-
+    st1.display();
+    st1.pop();
+    cout<<st1.peek()<<endl;
+    cout<<st1.countnodes()<<endl;
+    cout<< " The value to search for "<<endl;
+    cin>>item;
+    cout<<st1.isfound(item)<<endl;
+    st1.isfull();
 
 
     return 0;
